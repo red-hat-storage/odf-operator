4f0e8113f58954fa645101deabc6f0c84b79a83f
Jose A. Rivera <jarrpa@redhat.com>
2021-07-17 16:25:14 -0500

    makefile: split out help script
    Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

---
+++ hack/make-help.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+FILES=${@}
+
+awk 'BEGIN {
+       FS = ":.*##";
+       printf "\nUsage:\n  make \033[36m<target>\033[0m\n"
+     }
+
+     /^[a-zA-Z_0-9-]+:.*?##/ {
+       printf "  \033[36m%-15s\033[0m %s\n", $1, $2
+     }
+
+     /^##@/ {
+       printf "\n\033[1m%s\033[0m\n", substr($0, 5)
+     }' ${FILES}
