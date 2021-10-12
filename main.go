e0008616f8c1ce05fdc398efc0f424bf46bd4080
Jose A. Rivera <jarrpa@redhat.com>
2021-10-12 11:28:37 -0500

    Revert "controllers: remove setup for StorageCluster controller"
    This reverts commit 641f15dbdb83cdc0c5098a0d10017b116aedc29e.
    
    Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

--- main.go
+++ main.go
@@ -124,6 +124,16 @@ func main() {
 		os.Exit(1)
 	}
 
+	storageClusterReconciler := &controllers.StorageClusterReconciler{
+		Client:   mgr.GetClient(),
+		Scheme:   mgr.GetScheme(),
+		Recorder: controllers.NewEventReporter(mgr.GetEventRecorderFor("StorageCluster controller")),
+	}
+	if err = storageClusterReconciler.SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "StorageCluster")
+		os.Exit(1)
+	}
+
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
