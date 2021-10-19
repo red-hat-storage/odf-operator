0ec33a45eda51f6038a643315d90b1f967079beb
bipuladh <badhikar@redhat.com>
2021-10-19 21:47:14 +0530

    controllers: Create ClusterVersion controller scaffold
    operator-sdk create api --group config --version v1
    --kind ClusterVersion
    
    Signed-off-by: bipuladh <badhikar@redhat.com>

--- main.go
+++ main.go
@@ -144,6 +144,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "StorageCluster")
 		os.Exit(1)
 	}
+
+	if err = (&controllers.ClusterVersionReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ClusterVersion")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
