c69d625e410fde8acf0d64260fb715ea4919a3ef
Nitin Goyal <nigoyal@redhat.com>
2022-03-28 19:18:07 +0530

    makefile: update kustomize version
    Signed-off-by: Nitin Goyal <nigoyal@redhat.com>

--- hack/make-tools.mk
+++ hack/make-tools.mk
@@ -19,7 +19,7 @@ controller-gen: ## Download controller-gen locally if necessary.
 
 KUSTOMIZE = $(BIN_DIR)/kustomize
 kustomize: ## Download kustomize locally if necessary.
-	$(call go-get-tool,$(KUSTOMIZE),sigs.k8s.io/kustomize/kustomize/v3@v3.8.7)
+	$(call go-get-tool,$(KUSTOMIZE),sigs.k8s.io/kustomize/kustomize/v4@v4.5.3)
 
 OPERATOR_SDK = $(BIN_DIR)/operator-sdk
 operator-sdk: ## Download operator-sdk locally if necessary.
