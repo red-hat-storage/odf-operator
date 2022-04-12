7c47a41ef8e3cca06b5291d01ea7e6494361a7d9
Malay Kumar Parida <mparida@redhat.com>
2022-04-12 19:57:52 +0530

    test: Add the pkg to setup a test enviornment and cleanup
    Signed-off-by: Malay Kumar Parida <mparida@redhat.com>

---
+++ e2e/setup_teardown.go
@@ -0,0 +1,48 @@
+package e2e
+
+import (
+	"fmt"
+
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+)
+
+// Setup is the function called to initialize the test environment
+func Setup() {
+
+	debug("Setup: started\n")
+
+	SuiteFailed = true
+
+	if OdfOperatorInstall {
+		debug("Setup: deploying ODF Operator\n")
+		err := DeployManager.DeployODFWithOLM(OdfCatalogSourceImage, OdfSubscriptionChannel, OdfClusterServiceVersion)
+		gomega.Expect(err).To(gomega.BeNil())
+	}
+
+	SuiteFailed = false
+
+	debug("Setup: completed\n")
+}
+
+// TearDown is the function called to tear down the test environment
+func TearDown() {
+
+	debug("TearDown: started\n")
+
+	SuiteFailed = true
+
+	if OdfClusterUninstall {
+		debug("TearDown: uninstalling ODF Operator\n")
+		err := DeployManager.UndeployODFWithOLM(OdfCatalogSourceImage, OdfSubscriptionChannel, OdfClusterServiceVersion)
+		gomega.Expect(err).To(gomega.BeNil())
+	}
+
+	SuiteFailed = false
+
+	debug("TearDown: completed\n")
+}
+
+func debug(msg string, args ...interface{}) {
+	ginkgo.GinkgoWriter.Write([]byte(fmt.Sprintf(msg, args...))) //nolint:errcheck
+}
---
+++ pkg/deploymanager/namespace.go
@@ -0,0 +1,67 @@
+package deploymanager
+
+import (
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	utilwait "k8s.io/apimachinery/pkg/util/wait"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// CreateNamespace creates a namespace
+func (d *DeployManager) CreateNamespace(name string) error {
+
+	namespace := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+			Labels: map[string]string{
+				"openshift.io/cluster-monitoring": "true",
+			},
+		},
+	}
+
+	return d.Client.Create(d.Ctx, namespace)
+}
+
+// DeleteNamespaceAndWait deletes a namespace and waits on it to terminate
+func (d *DeployManager) DeleteNamespaceAndWait(name string) error {
+
+	namespace := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+			Labels: map[string]string{
+				"openshift.io/cluster-monitoring": "true",
+			},
+		},
+	}
+
+	err := d.Client.Delete(d.Ctx, namespace)
+	if err != nil && errors.IsNotFound(err) {
+		return nil
+	}
+
+	timeout := 600 * time.Second
+	interval := 10 * time.Second
+
+	err = utilwait.PollImmediate(interval, timeout, func() (done bool, err error) {
+
+		existingNamespace := &corev1.Namespace{}
+		err = d.Client.Get(d.Ctx, client.ObjectKeyFromObject(namespace), existingNamespace)
+		if err != nil && !errors.IsNotFound(err) {
+			return false, err
+		}
+		if err == nil {
+			d.Log.Info("Waiting on namespace to be deleted")
+			return false, nil
+		}
+		return true, err
+	})
+
+	if err != nil && !errors.IsNotFound(err) {
+		return err
+	}
+
+	return nil
+}
