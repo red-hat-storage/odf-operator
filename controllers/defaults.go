c46ca77a2bc39caddd0d02aaee546e19a3d1d2b5
Nitin Goyal <nigoyal@redhat.com>
2022-09-01 17:17:50 +0530

    controllers: update ocs-operator version
    Signed-off-by: Nitin Goyal <nigoyal@redhat.com>

--- controllers/defaults.go
+++ controllers/defaults.go
@@ -36,7 +36,7 @@ var (
 		"OCS_SUBSCRIPTION_NAME":                    "ocs-operator",
 		"OCS_SUBSCRIPTION_PACKAGE":                 "ocs-operator",
 		"OCS_SUBSCRIPTION_CHANNEL":                 "alpha",
-		"OCS_SUBSCRIPTION_STARTINGCSV":             "ocs-operator.v4.9.0",
+		"OCS_SUBSCRIPTION_STARTINGCSV":             "ocs-operator.v4.12.0",
 		"OCS_SUBSCRIPTION_CATALOGSOURCE":           "odf-catalogsource",
 		"OCS_SUBSCRIPTION_CATALOGSOURCE_NAMESPACE": "openshift-storage",
 
