16fd8ee4aaaf2c1fc45c76db646c30f28110b92c
Nitin Goyal <nigoyal@redhat.com>
2022-12-08 15:16:49 +0530

    api: add auto generated comment changes
    Signed-off-by: Nitin Goyal <nigoyal@redhat.com>

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the odf v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=odf.openshift.io
+// +kubebuilder:object:generate=true
+// +groupName=odf.openshift.io
 package v1alpha1
 
 import (
