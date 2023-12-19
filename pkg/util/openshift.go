440b88a092ff2101f9c1fa55f997b36fa72a2917
Leela Venkaiah G <lgangava@ibm.com>
2023-12-19 15:30:13 +0530

    controllers: surface dependent operator upgradeable conditions
    - odf-operator brings dependencies using OLM
    - this PR ensures odf-operator sets it's own operator condition based on
    upgradeability of it's dependents.
    
    Signed-off-by: Leela Venkaiah G <lgangava@ibm.com>

--- pkg/util/openshift.go
+++ pkg/util/openshift.go
@@ -20,6 +20,8 @@ import (
 	"context"
 
 	configv1 "github.com/openshift/api/config/v1"
+	operatorv2 "github.com/operator-framework/api/pkg/operators/v2"
+	"github.com/operator-framework/operator-lib/conditions"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -34,4 +36,20 @@ func DetermineOpenShiftVersion(client client.Client) (string, error) {
 		clusterVersion = version.Status.Desired.Version
 	}
 	return clusterVersion, nil
+}
+
+func getConditionFactory(client client.Client) conditions.Factory {
+	return conditions.InClusterFactory{Client: client}
+}
+
+func GetConditionName(client client.Client) (string, error) {
+	namespacedName, err := getConditionFactory(client).GetNamespacedName()
+	if err != nil {
+		return "", err
+	}
+	return namespacedName.Name, nil
+}
+
+func NewUpgradeableCondition(client client.Client) (conditions.Condition, error) {
+	return getConditionFactory(client).NewCondition(operatorv2.ConditionType(operatorv2.Upgradeable))
 }
