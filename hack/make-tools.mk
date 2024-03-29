728257ed18e82f9d865c24b029cab450099972a1
Malay Kumar Parida <mparida@redhat.com>
2024-03-29 06:04:40 +0000

    makefile: Fix panic in make generate in old versions of controller-gen
    Signed-off-by: Malay Kumar Parida <mparida@redhat.com>

--- hack/make-tools.mk
+++ hack/make-tools.mk
@@ -7,7 +7,7 @@ endef
 
 CONTROLLER_GEN = $(BIN_DIR)/controller-gen
 controller-gen: ## Download controller-gen locally if necessary.
-	$(call go-get-tool,$(CONTROLLER_GEN),sigs.k8s.io/controller-tools/cmd/controller-gen@v0.9.2)
+	$(call go-get-tool,$(CONTROLLER_GEN),sigs.k8s.io/controller-tools/cmd/controller-gen@v0.14.0)
 
 KUSTOMIZE = $(BIN_DIR)/kustomize
 kustomize: ## Download kustomize locally if necessary.
