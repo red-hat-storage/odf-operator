91f6a20cbfe3f99cef4d284a41830ef0a6909eff
Malay Kumar Parida <mparida@redhat.com>
2024-03-29 05:29:47 +0000

    Dummy Commit
    Signed-off-by: Malay Kumar Parida <mparida@redhat.com>

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ import (
 	"flag"
 	"os"
 
+	// Some dummy comment
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
