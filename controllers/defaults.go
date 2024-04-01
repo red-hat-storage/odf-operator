e1c94c0c1a8bd06a332b02ccbf33a318f5ec411a
Nitin Goyal <nigoyal@redhat.com>
2024-04-01 09:26:05 +0530

    makefile: update csi-addons version
    Signed-off-by: Nitin Goyal <nigoyal@redhat.com>

--- controllers/defaults.go
+++ controllers/defaults.go
@@ -48,7 +48,7 @@ var (
 		"CSIADDONS_SUBSCRIPTION_NAME":                    "csi-addons",
 		"CSIADDONS_SUBSCRIPTION_PACKAGE":                 "csi-addons",
 		"CSIADDONS_SUBSCRIPTION_CHANNEL":                 "alpha",
-		"CSIADDONS_SUBSCRIPTION_STARTINGCSV":             "csi-addons.v0.7.0",
+		"CSIADDONS_SUBSCRIPTION_STARTINGCSV":             "csi-addons.v0.8.0",
 		"CSIADDONS_SUBSCRIPTION_CATALOGSOURCE":           "odf-catalogsource",
 		"CSIADDONS_SUBSCRIPTION_CATALOGSOURCE_NAMESPACE": "openshift-marketplace",
 
