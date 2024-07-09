b9d5637e21a014a35a364402bf3d83a9d6b5c696
SanjalKatiyar <sanjaldhir@gmail.com>
2024-07-09 17:04:48 +0530

    console: update compatibility support matrix for 4.17
    Signed-off-by: SanjalKatiyar <sanjaldhir@gmail.com>

--- console/console.go
+++ console/console.go
@@ -133,7 +133,7 @@ func GetConsolePluginCR(consolePort int, serviceNamespace string) *consolev1.Con
 }
 
 func GetBasePath(clusterVersion string) string {
-	if strings.Contains(clusterVersion, "4.17") {
+	if strings.Contains(clusterVersion, "4.18") {
 		return COMPATIBILITY_BASE_PATH
 	}
 
