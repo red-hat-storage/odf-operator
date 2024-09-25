b1df424255f97aed33689e88e918cdc7609d8b84
Nikhil-Ladha <nikhilladha1999@gmail.com>
2024-09-25 19:53:33 +0530

    makefile: pin gingko version in tools script
    pin ginkgo version to same as go.mod file in
    tool script
    
    Signed-off-by: Nikhil-Ladha <nikhilladha1999@gmail.com>

--- hack/make-tools.mk
+++ hack/make-tools.mk
@@ -15,7 +15,7 @@ kustomize: ## Download kustomize locally if necessary.
 
 GINKGO = $(BIN_DIR)/ginkgo
 ginkgo: ## Download ginkgo locally if necessary.
-	$(call go-get-tool,$(GINKGO),github.com/onsi/ginkgo/v2/ginkgo@latest)
+	$(call go-get-tool,$(GINKGO),github.com/onsi/ginkgo/v2/ginkgo@v2.11.0)
 
 OPERATOR_SDK = $(BIN_DIR)/operator-sdk
 operator-sdk: ## Download operator-sdk locally if necessary.
