2d12c327d7bf18de50f61bf3bbddc552424c105f
Nitin Goyal <nigoyal@redhat.com>
2025-01-15 13:18:02 +0530

    controllers: create a csv webhook
    Signed-off-by: Nitin Goyal <nigoyal@redhat.com>

--- controllers/subscription_controller.go
+++ controllers/subscription_controller.go
@@ -22,6 +22,7 @@ import (
 
 	"github.com/go-logr/logr"
 	"go.uber.org/multierr"
+	admrv1 "k8s.io/api/admissionregistration/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -47,8 +48,10 @@ type SubscriptionReconciler struct {
 	client.Client
 	Scheme            *runtime.Scheme
 	Recorder          *EventReporter
+	OperatorNamespace string
 	ConditionName     string
 	OperatorCondition conditions.Condition
+	ctx               context.Context
 }
 
 //+kubebuilder:rbac:groups=operators.coreos.com,resources=subscriptions,verbs=get;list;watch;create;update;patch;delete
@@ -57,11 +60,13 @@ type SubscriptionReconciler struct {
 //+kubebuilder:rbac:groups=operators.coreos.com,resources=installplans,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=operators.coreos.com,resources=clusterserviceversions/finalizers,verbs=update
 //+kubebuilder:rbac:groups=operators.coreos.com,resources=operatorconditions,verbs=get;list;watch
+//+kubebuilder:rbac:groups=admissionregistration.k8s.io,resources=mutatingwebhookconfigurations,verbs=get;list;watch;create;update
 
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *SubscriptionReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 
+	r.ctx = ctx
 	logger := log.FromContext(ctx)
 
 	instance := &operatorv1alpha1.Subscription{}
@@ -75,12 +80,17 @@ func (r *SubscriptionReconciler) Reconcile(ctx context.Context, req ctrl.Request
 		return ctrl.Result{}, err
 	}
 
-	err = r.setOperatorCondition(logger, req.NamespacedName.Namespace)
+	err = r.setOperatorCondition(logger)
 	if err != nil {
 		return ctrl.Result{}, err
 	}
 
-	err = r.ensureSubscriptions(logger, req.NamespacedName.Namespace)
+	err = ensureWebhook(r.ctx, r.Client, logger, r.OperatorNamespace)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.ensureSubscriptions(logger)
 	if err != nil {
 		return ctrl.Result{}, err
 	}
@@ -88,7 +98,7 @@ func (r *SubscriptionReconciler) Reconcile(ctx context.Context, req ctrl.Request
 	return ctrl.Result{}, nil
 }
 
-func (r *SubscriptionReconciler) ensureSubscriptions(logger logr.Logger, namespace string) error {
+func (r *SubscriptionReconciler) ensureSubscriptions(logger logr.Logger) error {
 
 	var err error
 
@@ -96,7 +106,7 @@ func (r *SubscriptionReconciler) ensureSubscriptions(logger logr.Logger, namespa
 	subsList[StorageClusterKind] = GetSubscriptions(StorageClusterKind)
 
 	ssList := &odfv1alpha1.StorageSystemList{}
-	err = r.Client.List(context.TODO(), ssList, &client.ListOptions{Namespace: namespace})
+	err = r.Client.List(context.TODO(), ssList, &client.ListOptions{Namespace: r.OperatorNamespace})
 	if err != nil {
 		return err
 	}
@@ -137,9 +147,9 @@ func (r *SubscriptionReconciler) ensureSubscriptions(logger logr.Logger, namespa
 	return err
 }
 
-func (r *SubscriptionReconciler) setOperatorCondition(logger logr.Logger, namespace string) error {
+func (r *SubscriptionReconciler) setOperatorCondition(logger logr.Logger) error {
 	ocdList := &operatorv2.OperatorConditionList{}
-	err := r.Client.List(context.TODO(), ocdList, client.InNamespace(namespace))
+	err := r.Client.List(context.TODO(), ocdList, client.InNamespace(r.OperatorNamespace))
 	if err != nil {
 		logger.Error(err, "failed to list OperatorConditions")
 		return err
@@ -290,12 +300,25 @@ func (r *SubscriptionReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		},
 	)
 
+	enqueueOdfSub := handler.EnqueueRequestsFromMapFunc(
+		func(ctx context.Context, obj client.Object) []reconcile.Request {
+			logger := log.FromContext(ctx)
+			sub, err := GetOdfSubscription(r.Client)
+			if err != nil {
+				logger.Error(err, "failed to get ODF Subscription")
+				return []reconcile.Request{}
+			}
+			return []reconcile.Request{{NamespacedName: types.NamespacedName{Name: sub.Name, Namespace: sub.Namespace}}}
+		},
+	)
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&operatorv1alpha1.Subscription{},
 			builder.WithPredicates(generationChangedPredicate, subscriptionPredicate)).
 		Owns(&operatorv1alpha1.Subscription{},
 			builder.WithPredicates(generationChangedPredicate)).
 		Watches(&operatorv2.OperatorCondition{}, enqueueFromCondition, builder.WithPredicates(conditionPredicate)).
+		Watches(&admrv1.MutatingWebhookConfiguration{}, enqueueOdfSub, builder.WithPredicates(generationChangedPredicate)).
 		Complete(r)
 }
 
