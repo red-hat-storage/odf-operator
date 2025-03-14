d87a45d78ee7ec490248e001d6641ee24392fb4c
Nitin Goyal <nigoyal@redhat.com>
2025-03-14 12:08:59 +0530

    controllers: rename i to j
    i is already being used in above loop (removing variable shadowing)
    
    Signed-off-by: Nitin Goyal <nigoyal@redhat.com>

--- controllers/operatorscaler_controller.go
+++ controllers/operatorscaler_controller.go
@@ -211,8 +211,8 @@ func (r *OperatorScalerReconciler) reconcileOperators() error {
 					r.log.Error(err, "failed listing csvs with label", "label", key)
 					multierr.AppendInto(&returnErr, err)
 				} else {
-					for i := range csvList.Items {
-						if err = r.updateCsvDeplymentsReplicas(&csvList.Items[i]); err != nil {
+					for j := range csvList.Items {
+						if err = r.updateCsvDeplymentsReplicas(&csvList.Items[j]); err != nil {
 							r.log.Error(err, "failed updating csvs replica")
 							multierr.AppendInto(&returnErr, err)
 						}
