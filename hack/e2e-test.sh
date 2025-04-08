53d81f80c7018010cae0726dd3f27823c6812e61
Nitin Goyal <nigoyal@redhat.com>
2025-04-08 13:01:13 +0530

    test: remove the variables for csv and have a list
    Signed-off-by: Nitin Goyal <nigoyal@redhat.com>

--- hack/e2e-test.sh
+++ hack/e2e-test.sh
@@ -8,12 +8,4 @@ cd e2e/odf && ${GINKGO} build && ./odf.test \
     --odf-operator-install=${ODF_OPERATOR_INSTALL} \
     --odf-operator-uninstall=${ODF_OPERATOR_UNINSTALL} \
     --odf-cluster-service-version=odf-operator.v${VERSION} \
-    --odf-deps-cluster-service-version=odf-dependencies.v${VERSION} \
-    --ocs-cluster-service-version=${OCS_SUBSCRIPTION_STARTINGCSV} \
-    --ocs-client-cluster-service-version=${OCS_CLIENT_SUBSCRIPTION_STARTINGCSV} \
-    --nooba-cluster-service-version=${NOOBAA_SUBSCRIPTION_STARTINGCSV} \
-    --csiaddons-cluster-service-version=${CSIADDONS_SUBSCRIPTION_STARTINGCSV} \
-    --cephcsi-cluster-service-version=${CEPHCSI_SUBSCRIPTION_STARTINGCSV} \
-    --rook-cluster-service-version=${ROOK_SUBSCRIPTION_STARTINGCSV} \
-    --prometheus-cluster-service-version=${PROMETHEUS_SUBSCRIPTION_STARTINGCSV} \
-    --recipe-cluster-service-version=${RECIPE_SUBSCRIPTION_STARTINGCSV}
+    --csv-names="${CSV_NAMES}"
