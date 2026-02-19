329d9585149e718d15edcc01d6423b05d0f24a0c
Nitin Goyal <nigoyal@redhat.com>
2026-02-19 14:31:33 +0530

    add controller scaffolding
    kubebuilder create api \
        --resource=false --controller=true \
        --version=v1 --kind=ClusterExtension \
        --group=olm --external-api-domain=operatorframework.io \
        --external-api-module=github.com/operator-framework/operator-controller \
        --external-api-path=github.com/operator-framework/operator-controller/api/v1
    
    Signed-off-by: Nitin Goyal <nigoyal@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -25,6 +25,7 @@ import (
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
+	olmv1 "github.com/operator-framework/operator-controller/api/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -34,6 +35,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	"github.com/red-hat-storage/odf-operator/internal/controller"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -45,6 +48,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(olmv1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -174,6 +178,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err := (&controller.ClusterExtensionReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ClusterExtension")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
