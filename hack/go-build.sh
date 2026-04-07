e252e13ba335b2c3c3fdfeebdb7d9a6cd7e01342
AnikaYadav <Anika.Yadav@ibm.com>
2026-04-07 18:25:27 +0530

    dockerfile: rename manager binary to odf-operator
    Signed-off-by: AnikaYadav <Anika.Yadav@ibm.com>

--- hack/go-build.sh
+++ hack/go-build.sh
@@ -7,5 +7,5 @@ export GO111MODULE=${GO111MODULE:-on}
 
 set -x
 
-go build -a -o ${GOBIN:-bin}/manager main.go
+go build -a -o ${GOBIN:-bin}/odf-operator main.go
 go build -a -o ${GOBIN:-bin}/ux-backend-server services/ux-backend/main.go
